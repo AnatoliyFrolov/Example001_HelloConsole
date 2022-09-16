5e13ea3bd89c2682ffbb8618ed3d9ded0898608f
anatoliy_frolov <anatoliyfrolovmusic@gmail.com>
2022-09-16 11:08:17 +0300

    Add If Else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+   Console.WriteLine("Наконец-то Маша пришла");
+}
+else
+{
+Console.Write("Привет, ");
+Console.WriteLine(username);
+}
