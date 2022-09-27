ebea7c63e919f3305f854e3288a61071bd36cfec
anatoliy_frolov <anatoliyfrolovmusic@gmail.com>
2022-09-27 20:43:29 +0300

    Finish

---
+++ HomeWork Lection 1-1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine ("Введите первое число ");
+string input1 = Console.ReadLine();
+int number1  = int.Parse(input1);
+
+Console.WriteLine ("Введите второе число ");
+string input2 = Console.ReadLine();
+int number2  = int.Parse(input2);
+
+if (number1 > number2)
+{
+    Console.WriteLine("Максимальное число ");
+    Console.WriteLine(number1);
+    Console.WriteLine("Минимальное число ");
+    Console.WriteLine(number2);
+}
+else 
+{
+    Console.WriteLine("Максимальное число ");
+    Console.WriteLine(number2);
+    Console.WriteLine("Минимальное число ");
+    Console.WriteLine(number1);
+}
---
+++ HomeWork Lection 1-2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine ("Введите первое число ");
+string input1 = Console.ReadLine();
+int number1  = int.Parse(input1);
+
+Console.WriteLine ("Введите второе число ");
+string input2 = Console.ReadLine();
+int number2  = int.Parse(input2);
+
+Console.WriteLine ("Введите третье число ");
+string input3 = Console.ReadLine();
+int number3  = int.Parse(input3);
+
+int Max = number1;
+
+if (number2 > number1)
+{
+    Max = number2;
+}
+if (number3 > number1)
+{
+   Max = number3;
+}
+
+Console.WriteLine ("Максимальное число ");
+Console.WriteLine (Max);
