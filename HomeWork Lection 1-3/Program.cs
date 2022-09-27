e4987e4dc3a22a9c4f2a8b803ad6a7dc8512c933
anatoliy_frolov <anatoliyfrolovmusic@gmail.com>
2022-09-27 20:45:52 +0300

    Last update

---
+++ HomeWork Lection 1-3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine ("Введите первое число ");
+string input1 = Console.ReadLine();
+int number1  = int.Parse(input1);
+
+if (number1 % 2 == 0)
+{
+    Console.WriteLine ("Данное число четное ");
+}
+
+else
+{
+   Console.WriteLine ("Данное число нечетное ");
+}
